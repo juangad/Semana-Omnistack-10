4f9a55a6335e6c3743513c98f6ebc874ddd4835d
Juan Garcia <46980640+juangad@users.noreply.github.com>
2020-01-29 07:19:27 -0300

    Adição do Mobile
    Commit do desenvolvimento inicial do módulo mobile.

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps'
+import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
+
+function Main({ navigation }) {
+  const [currentRegion, setCurrentRegion ] = useState(null)
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync()
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        })
+
+        const { latitude, longitude } = coords
+        
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })        
+      }
+
+    }
+
+    loadInitialPosition()
+  }, [])
+
+  if (!currentRegion) {
+    return null
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{latitude: -22.8658567, longitude: -43.0973265}}>
+        <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/46980640?s=460&v=4'}} />
+
+        <Callout onPress={ () => {
+          navigation.navigate('Profile', { github_username: 'juangad' })
+        }}>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Juan Garcia</Text>
+            <Text style={styles.devBio}>Imposto é roubo</Text>
+            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  )
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF',
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
+  },
+})
+
+export default Main
